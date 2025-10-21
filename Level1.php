156ba86f9254465f8521f7961b80f9c528c304ab
Lucas <lxcxsito@gmail.com>
2025-10-21 13:39:28 +0200

    Primera Subida

---
+++ Level1.php
@@ -0,0 +1,83 @@
+<?php 
+
+//TASK 1 
+class Animal{
+    public $nom;
+    public Llenguatge $parlen ;
+
+    public function __construct($nom, Llenguatge $parlen){
+        $this->nom = $nom;
+        $this->parlen = $parlen;
+    }
+
+}
+
+
+
+
+class Llenguatge{
+    public $nom;
+    public function __construct($nom){
+        $this->nom = $nom;
+    }
+}
+
+$maullar = new Llenguatge("Maullar");
+$ladrar = new Llenguatge("Ladrar");
+
+
+$animal1 = new Animal("Gat", $maullar);
+$animal2 = new Animal("Gos", $ladrar);
+
+
+
+
+
+//TASK 2
+
+abstract class Shape 
+{
+
+    //Atributos
+    public $height;
+    public $width;
+
+    function __construct($height, $width)
+    {
+        $this->height = $height;
+        $this->width = $width;
+    }
+
+     abstract function calculateArea();
+
+
+
+}
+
+class Triangle extends Shape
+{
+
+    public function calculateArea()
+    {
+        return ($this->height * $this->width) / 2;
+    }
+
+}
+
+class Rectangle extends Shape
+{
+
+    public function calculateArea()
+    {
+        return $this->height * $this->width;
+    }
+
+}
+
+//PRUEBAS LEVEL 1 - TASK 2
+$triangle = new Triangle(10, 15);
+echo "El área del triángulo es: " . $triangle->calculateArea() . "\n";
+$rectangle = new Rectangle(10, 15);
+echo "El área del rectángulo es: " . $rectangle->calculateArea() . "\n";
+
+?>
